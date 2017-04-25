87278b82c4f763207843e83b51d9aac99182fbd4
Matusalem Marques <mmar@matuslab.net>
2017-04-25 18:17:08 +0100

    Problem 8.

--- Euler Playgrounds.playground/Pages/Project Euler.xcplaygroundpage/Contents.swift
+++ Euler Playgrounds.playground/Pages/Project Euler.xcplaygroundpage/Contents.swift
@@ -23,5 +23,7 @@ Visit the [Project Euler website](https://projecteuler.net/)
 [Problem 6](Problem%206) - Sum square difference
 
 [Problem 7](Problem%207) - 10001st prime
+ 
+[Problem 8](Problem%208) - Largest product in a series
 
  */
